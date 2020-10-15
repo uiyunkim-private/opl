b8629df2d47a07a61351307b71c4c00b73075520
Uiyun Kim <uiyun_kim@student.uml.edu>
2020-10-16 01:32:53 +0900

    up

--- hw1/main.go
+++ hw1/main.go
@@ -1,21 +1,19 @@
-// This code is from Rosettacode.org
-// http://rosettacode.org/wiki/FizzBuzz
-// This go program will use switch strategy to play fizzbuzz
 package main
  
 import "fmt"
  
 func main() {
-    for i := 1; i <= 100; i++ { // Swtich
-        switch {
-        case i%15==0: //case both mod 3 and mod 5 simultaneoulsy.
-            fmt.Println("FizzBuzz")
-        case i%3==0:
-            fmt.Println("Fizz")
-        case i%5==0:
-            fmt.Println("Buzz")
-        default: 
-            fmt.Println(i)
+    for i := 1; i <= 100; i++ {
+        if i%3==0 {
+            fmt.Printf("Fizz")
+            if i%5==0{
+                fmt.Printf("Buzz")
+            }
+        } else if i%5==0 {
+            fmt.Printf("Buzz")
+        } else {
+            fmt.Printf("%v",i)
         }
+        fmt.Printf("\n")
     }
 }
